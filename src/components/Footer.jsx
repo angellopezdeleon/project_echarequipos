fccb5dd3f4153e0d31bf24117064a414ac3c25f7
angel <angel.lopjar@gmail.com>
2023-02-24 13:48:50 +0100

    Added link to Linkeding in the footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,33 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 
 const pStyle = {
-  color: 'white',
+    color: "white",
 };
 
 export default function Footer() {
-  return (
-    <div>
-      <Box
-        sx={{
-          width: "100%",
-          height: "50px",
-          backgroundColor: "#0d47a1",
-          display: "flex",
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <p style={pStyle}>Desarrollado por Ángel López de León</p>
-      </Box>
-    </div>
-  );
+    return (
+        <div>
+            <Box
+                sx={{
+                    width: "100%",
+                    height: "50px",
+                    backgroundColor: "#0d47a1",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <p style={pStyle}>
+                    Desarrollado por{" "}
+                    <a
+                        style={pStyle}
+                        target="_blank"
+                        href="https://www.linkedin.com/in/angel-lopez-de-leon-jaramillo/"
+                    >
+                        Ángel López de León
+                    </a>
+                </p>
+            </Box>
+        </div>
+    );
 }
